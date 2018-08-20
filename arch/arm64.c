bhupesh-sharma/makedumpfile

commit 94c97db3fe859ca14d7b38b0ae9ee0ffb83689d2

[PATCH] arm64: Get 'info->page_offset' from PT_LOAD segments to support KASLR boot cases
The existing methodology to obtain 'info->page_offset' from reading
_stext symbol (from kallsyms) doesn't work well in KASLR boot cases on
arm64 machines as the PAGE_OFFSET (or the virtual address which
indicates the start of the linear region) can be randomized as well
on basis of the kaslr-seed.

Since the value of PAGE_OFFSET inside the kernel is randomized in such
cases and there is no existing mechanism of conveying this value from
kernel-space to user-space, so we can use the method used by archs like
x86_64 to generate the 'info->page_offset' value from the PT_LOAD
segments by subtracting the phy_addr from virt_addr of a PT_LOAD
segment.

This approach works fine both with KASLR and non-KASLR boot cases.

I tested this on my qualcomm-amberwing board. Here are some logs from
the KASLR boot cases:

- Verify that the EFI firmware supports 'kaslr-seed':

	chosen {
		kaslr-seed = <0x0 0x0>;
		<..snip..>
	};

- Verify that '--mem-usage' works well after this fix as well (I used
  kernel 4.18.0-rc4+ for my checks):

The kernel version is not supported.
The makedumpfile operation may be incomplete.

TYPE		PAGES			EXCLUDABLE	DESCRIPTION
----------------------------------------------------------------------
ZERO		4396            	yes		Pages filled with zero
NON_PRI_CACHE	27859           	yes		Cache pages without private flag
PRI_CACHE	18490           	yes		Cache pages with private flag
USER		2728            	yes		User process pages
FREE		1465848         	yes		Free pages
KERN_DATA	18537           	no		Dumpable kernel data

page size:		65536
Total pages on system:	1537858
Total size on system:	100785061888     Byte

Signed-off-by: Bhupesh Sharma <bhsharma@redhat.com>
Signed-off-by: Kazuhito Hagio <k-hagio@ab.jp.nec.com>

arch/arm64.c
@@ -265,6 +265,9 @@ get_xen_info_arm64(void)
 int
 get_versiondep_info_arm64(void)
 {
+	int i;
+	unsigned long long phys_start;
+	unsigned long long virt_start;
 	ulong _stext;
 
 	_stext = get_stext_symbol();
@@ -289,8 +292,23 @@ get_versiondep_info_arm64(void)
 		return FALSE;
 	}
 
-	info->page_offset = (0xffffffffffffffffUL) << (va_bits - 1);
+	if (get_num_pt_loads()) {
+		for (i = 0;
+		    get_pt_load(i, &phys_start, NULL, &virt_start, NULL);
+		    i++) {
+			if (virt_start != NOT_KV_ADDR
+			    && virt_start < __START_KERNEL_map
+			    && phys_start != NOT_PADDR
+			    && phys_start != NOT_PADDR_ARM64) {
+				info->page_offset = virt_start - phys_start;
+				DEBUG_MSG("info->page_offset: %lx, VA_BITS: %d\n",
+						info->page_offset, va_bits);
+				return TRUE;
+			}
+		}
+	}
 
+	info->page_offset = (0xffffffffffffffffUL) << (va_bits - 1);
 	DEBUG_MSG("page_offset=%lx, va_bits=%d\n", info->page_offset,
 			va_bits);
 

makedumpfile.h
@@ -542,6 +542,10 @@ do { \
 #ifdef __aarch64__
 unsigned long get_kvbase_arm64(void);
 #define KVBASE			get_kvbase_arm64()
+
+#define __START_KERNEL_map	(0xffffffff80000000UL)
+#define NOT_PADDR_ARM64		(0x0000000010a80000UL)
+
 #endif /* aarch64 */
 
 #ifdef __arm__

commit b68476bbfb791ac98bd823052de587862ae74f10

arm64: Add a generic method of determining invalid phy-addr for calculating 'info->page_offset'
Commit 94c97db3fe859ca14d7b38b0ae9ee0ffb83689d2 (arm64: Get
'info->page_offset' from PT_LOAD segments to support KASLR boot cases)
added a method to determine 'info->page_offset' from PT_LOAD segments
for arm64 platforms.

In this commit we hardcoded the NOT_PADDR_ARM64 macro as
0x0000000010a80000UL which was a valid value on
qualcomm-amberwing boards.

However, I was testing this change on several other arm64 boards
like apm-mustang, huawei-taishan and hp-moonshot and saw that
this value can vary on the basic of the "Kernel code" memory range
placement.

This patch determines the "Kernel code" memory range from
'/proc/iomem' listing and uses the same to determine the
NOT_PADDR_ARM64 value, which makes this calculation platform
independent and this works well on apm-mustang, huawei-taishan,
qualcomm-amberwing and hp-moonshot platforms.

Here are some logs on my huawei-taishan board with kernel version
4.18.0-rc8:

The kernel version is not supported.
The makedumpfile operation may be incomplete.

TYPE		PAGES			EXCLUDABLE	DESCRIPTION
----------------------------------------------------------------------
ZERO		83081           	yes		Pages filled with zero
NON_PRI_CACHE	6330            	yes		Cache pages without private flag
PRI_CACHE	927             	yes		Cache pages with private flag
USER		2659            	yes		User process pages
FREE		4053998         	yes		Free pages
KERN_DATA	35331           	no		Dumpable kernel data

page size:		65536
Total pages on system:	4182326
Total size on system:	274092916736     Byte

Fixes: 94c97db3fe859ca14d7b38b0ae9ee0ffb83689d2 (arm64: Get
	'info->page_offset' from PT_LOAD segments to support KASLR boot
	cases)

Signed-off-by: Bhupesh Sharma <bhsharma@redhat.com>

arch/arm64.c
@@ -303,10 +303,25 @@ get_xen_info_arm64(void)
 	return ERROR;
 }
 
+/* We want to exclude the kernel code mem region while calculating the
+ * info->page_offset for arm64 arch
+ */
+static struct memory_range kernel_code_mem_range;
+
+static int kernel_code_resource_callback(void *data, int nr,
+					  char *str,
+					  unsigned long base,
+					  unsigned long length)
+{
+	kernel_code_mem_range.start = base;
+	kernel_code_mem_range.end   = base + length - 1;
+	return 0;
+}
+
 int
 get_versiondep_info_arm64(void)
 {
-	int i;
+	int i, ret;
 	unsigned long long phys_start;
 	unsigned long long virt_start;
 	ulong _stext;
@@ -333,6 +348,13 @@ get_versiondep_info_arm64(void)
 		return FALSE;
 	}
 
+	ret = iomem_for_each_line("Kernel code\n",
+					kernel_code_resource_callback, NULL);
+	if (ret != 1) {
+		ERRMSG("Cannot find a proper Kernel code memory range in /proc/iomem\n");
+		return FALSE;
+	}
+
 	if (get_num_pt_loads()) {
 		for (i = 0;
 		    get_pt_load(i, &phys_start, NULL, &virt_start, NULL);
@@ -340,7 +362,7 @@ get_versiondep_info_arm64(void)
 			if (virt_start != NOT_KV_ADDR
 			    && virt_start < __START_KERNEL_map
 			    && phys_start != NOT_PADDR
-			    && phys_start != NOT_PADDR_ARM64) {
+			    && phys_start != kernel_code_mem_range.start) {
 				info->page_offset = virt_start - phys_start;
 				DEBUG_MSG("info->page_offset: %lx, VA_BITS: %d\n",
 						info->page_offset, va_bits);

makedumpfile.h
@@ -544,7 +544,6 @@ unsigned long get_kvbase_arm64(void);
 #define KVBASE			get_kvbase_arm64()
 
 #define __START_KERNEL_map	(0xffffffff80000000UL)
-#define NOT_PADDR_ARM64		(0x0000000010a80000UL)
 
 #endif /* aarch64 */
 